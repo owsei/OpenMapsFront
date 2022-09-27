06508b35260c5bf1f37c960ff23292c6c278ca4d
Pedro Esparza <owsei@yahoo.com>
2022-09-28 01:10:28 +0200

    Primer Commit

---
+++ src/app/open-maps/open-maps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-maps',
+  templateUrl: './open-maps.component.html',
+  styleUrls: ['./open-maps.component.css']
+})
+export class OpenMapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
